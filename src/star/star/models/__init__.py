e9824d5d50adf389ccabd8207fa387b6b2d00add
Chung-Yi Chi <cychi1210@gmail.com>
2013-07-14 22:26:42 -0400

    Add db_add and db_remove function in the model __init__ script.

--- src/star/star/models/__init__.py
+++ src/star/star/models/__init__.py
@@ -1,3 +1,18 @@
+from star import db
 import datetime
 
 now = datetime.datetime.now
+
+def db_add(obj, commit=True):
+    db.session.add(obj)
+    db.session.flush()
+    if commit:
+        db.session.commit()
+    return obj
+
+def db_remove(obj, commit=True):
+    db.session.delete(obj)
+    db.session.flush()
+    if commit:
+        db.session.commit()
+    return True

a425188ba2e342561eeae5cb27bb477c9a62f6cd
Chung-Yi Chi <cychi1210@gmail.com>
2013-07-26 16:30:13 -0400

    Add comments to the taxonomy model class.

--- src/star/star/models/core/taxonomy.py
+++ src/star/star/models/core/taxonomy.py
@@ -2,6 +2,9 @@ from star import db
 from star.models import now
 
 class Term(db.Model):
+    """
+    The basic information about a single term. It's not meaningful without context. 
+    """
     __tablename__ = 'term'
     id =            db.Column(db.Integer, db.Sequence('term_seq_id', optional=True), primary_key=True)
     name =          db.Column(db.Unicode(149), nullable=False, unique=True)
@@ -9,6 +12,9 @@ class Term(db.Model):
     created =       db.Column(db.DateTime, nullable=False, default=now)
 
 class TaxonomyTerm(db.Model):
+    """
+    A term is not a category or tag on its own. It must be given context via the taxonomy.
+    """
     __tablename__ = 'taxonomy_term'
     term_id =       db.Column(db.Integer, db.ForeignKey('term.id', ondelete='CASCADE'), primary_key=True)
     taxonomy_id =   db.Column(db.Integer, db.ForeignKey('taxonomy.id', ondelete='CASCADE'), primary_key=True)
@@ -18,6 +24,9 @@ class TaxonomyTerm(db.Model):
     created =       db.Column(db.DateTime, nullable=False, default=now)
 
 class Taxonomy(db.Model):
+    """
+    A taxonomy is a way to group posts together, e.g. "tag" and "category".
+    """
     __tablename__ = 'taxonomy'
     id =        db.Column(db.Integer, db.Sequence('taxonomy_seq_id', optional=True), primary_key=True)
     name =      db.Column(db.Unicode(149), nullable=False, unique=True)

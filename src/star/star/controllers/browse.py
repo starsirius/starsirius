827ddaea4a3cf8698e8528e2607f2268c42a009f
Chung-Yi Chi <cychi1210@gmail.com>
2013-08-27 11:16:15 -0400

    Add browse pages for blog, ux, and front-end development.

--- src/star/star/controllers/browse.py
+++ src/star/star/controllers/browse.py
@@ -4,8 +4,24 @@ from star.views import render
 from star.views import browse as browse_views
 from star.models.dal.post import PostPrivateDAL
 
+@app.route('/blog', methods=['GET'])
+def browse_blog():
+    data = {}
+    return render(browse_views.browse_blog, data)
+
+@app.route('/ux', methods=['GET'])
+def browse_ux_posts():
+    data = {}
+    return render(browse_views.browse_ux_posts, data)
+
+@app.route('/front-end-development', methods=['GET'])
+def browse_front_end_development_posts():
+    data = {}
+    return render(browse_views.browse_front_end_development_posts, data)
+
 @app.route('/portfolio', methods=['GET'])
 def browse_portfolio():
     works = PostPrivateDAL.getWorks()
     data = {"works": works}
     return render(browse_views.browse_portfolio, data)
+
--- src/star/star/views/browse.py
+++ src/star/star/views/browse.py
@@ -1,5 +1,35 @@
 from flask import render_template, render_template_string
 
+def browse_blog(data_dict):
+    """
+    Generates the html string for the blog page given a data dictionary.
+    Arguments:
+        data_dict - a dictionary, the data to be used in the html template
+    Return:
+        res - a string, the html string of the view
+    """
+    return render_template('templates/blog-browse.html', **data_dict)
+
+def browse_ux_posts(data_dict):
+    """
+    Generates the html string for the UX page given a data dictionary.
+    Arguments:
+        data_dict - a dictionary, the data to be used in the html template
+    Return:
+        res - a string, the html string of the view
+    """
+    return render_template('templates/ux-browse.html', **data_dict)
+
+def browse_front_end_development_posts(data_dict):
+    """
+    Generates the html string for the front-end dev page given a data dictionary.
+    Arguments:
+        data_dict - a dictionary, the data to be used in the html template
+    Return:
+        res - a string, the html string of the view
+    """
+    return render_template('templates/front-end-dev-browse.html', **data_dict)
+
 def browse_portfolio(data_dict):
     """
     Generates the html string for the portfolio page given a data dictionary.
@@ -9,3 +39,4 @@ def browse_portfolio(data_dict):
         res - a string, the html string of the view
     """
     return render_template('templates/portfolio-browse.html', **data_dict)
+
